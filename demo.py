ab98535e2db5a46b44007a4f4ce4d0ba600fc0ca
bruno <levybruno@gmail.com>
2021-06-18 17:21:01 -0300

    simulating return series

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
-print('hello')
 
-print('welcome')
+import numpy as np
+import pandas as pd
+
+retornos = pd.DataFrame()
+
+for i in range(10):
+    retornos['Ret'+ str(i) ] =  np.random.standard_normal(1000)
+
+
+
+
