148a52e9b25bf55a979431dd83ec4c3a908fa0a9
bruno <levybruno@gmail.com>
2021-06-21 15:33:55 -0300

    simulating tsmom data

--- TSMOM.py
+++ TSMOM.py
@@ -1,18 +1,18 @@
 import numpy as np
 import pandas as pd
 import functions as fc
-
+from datetime import datetime
 
 
 #factors, retornos = fc.simulate_rets(k=3, p=100,n=1000)
 #sr_factors = factors.mean()*np.sqrt(252)/np.std(factors)
 
 start = "1/1/2010"
-end   = datetime.date(datetime.now())
+end = datetime.date(datetime.now())
 
 retornos = fc.simulate_rets_mom(p=100,start=start, end=end)
 sr_rets = retornos.mean()*np.sqrt(252)/np.std(retornos)
 
-ret_tsmom = fc.naive_tsmom(retornos, look_back = 252, vol_target=0.4)
+ret_tsmom = fc.naive_tsmom(retornos, look_back = 52, vol_target=0.4)
 
 sr_tsmom = ret_tsmom.mean()*np.sqrt(252)/np.std(ret_tsmom)
--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def simulate_rets_factors(k=3, p=10,n=1000):
@@ -44,31 +44,31 @@ def simulate_rets_mom(p,start, end):
     return retornos
 
 
-def naive_tsmom(rets, look_back = 252, vol_target=0.4):
+def naive_tsmom(rets, look_back = 52, vol_target=0.4):
 
-    prices = (1+rets).cumprod()
-    vols = pd.DataFrame(index=rets.index)
+    prices = (1+rets).cumprod().resample('W', loffset=pd.offsets.timedelta(days=-2)).last()
+    ret_new = prices.pct_change(1)
+    vols = pd.DataFrame(index=ret_new.index)
     df_tsmom = prices.pct_change(look_back)
-    df_strat = pd.DataFrame(index=rets.index, columns=rets.columns)
+    df_strat = pd.DataFrame(index=ret_new.index, columns=ret_new.columns)
 
-    for i in rets.columns:
-        vols[str(i)] = rets[str(i)].ewm(ignore_na=False,
+    for i in ret_new.columns:
+        vols[str(i)] = ret_new[str(i)].ewm(ignore_na=False,
                         adjust=True,
                         com=60,
-                        min_periods=0).std(bias=False) * np.sqrt(252)
+                        min_periods=0).std(bias=False) * np.sqrt(52)
 
-        for t in rets.index:
-            if t > look_back:
+        for t in ret_new.index:
+            if t > ret_new.index[look_back-1]:
 
-                if t % 5 ==0:
-                    signal = np.sign(df_tsmom[str(i)].iloc[t - 1])
+                signal = np.sign(df_tsmom[str(i)].loc[t - timedelta(7)])
 
-                    if signal>=0:
-                        df_strat[str(i)].iloc[t] = (vol_target/vols[str(i)].iloc[t])*signal*rets[str(i)].iloc[t]
+                if signal>=0:
+                    df_strat[str(i)].loc[t] = (vol_target/vols[str(i)].loc[t - timedelta(7)])*signal*ret_new[str(i)].loc[t]
 
-                    else:
-                        df_strat[str(i)].iloc[t] = (vol_target / vols[str(i)].iloc[t]) *(signal* rets[str(i)].iloc[t])
                 else:
+                    df_strat[str(i)].loc[t] = (vol_target / vols[str(i)].loc[t - timedelta(7)]) *(signal* ret_new[str(i)].loc[t])
+
 
 
 
